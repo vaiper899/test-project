97016a281702e4893cf697fbcefda6a0e8eb895b
vaiper899 <vaiper899@hotmail.com>
2015-04-12 20:52:36 -0200

    Initial Commit

---
+++ src/com/dreamsyssoft/demo/rest/model/UserRequest.java
@@ -0,0 +1,13 @@
+package com.dreamsyssoft.demo.rest.model;
+
+public class UserRequest {
+    private User user;
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
